99d83a13db407b2ad656664baa37a75d78107c1a
Jason Kim <yong076@naver.com>
2015-02-23 18:00:18 +0900

    123123

--- server.rb
+++ server.rb
@@ -8,7 +8,7 @@ use Rack::Csrf
 
 enable :sessions
 set :bind, '0.0.0.0'
-set :session_secret,          ENV['k']
+set :session_secret,          ENV['String']
 set :slack_invite_api_url,    'https://slack.com/api/users.admin.invite'
 set :background_color,        ENV.fetch('BACKGROUND_COLOR', '#FDFCFB')
 set :text_color,              ENV.fetch('TEXT_COLOR', '#FDFCFB')
